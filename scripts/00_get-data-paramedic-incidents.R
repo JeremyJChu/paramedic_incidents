b8e081c1e9d50e7c332d06dddd6c6b25fa168b5c
Jeremy Chu <jeremychuj@gmail.com>
2021-01-25 20:48:32 -0500

    script changes 2

--- scripts/00_get-data-paramedic-incidents.R
+++ scripts/00_get-data-paramedic-incidents.R
@@ -21,8 +21,25 @@ paramedic_data <-
 #Saving only the relevant sheets for data analysis
 paramedic2020 <- paramedic_data[["2020"]]
 paramedic2019 <- paramedic_data[["2019"]]
-
+paramedic2018 <- paramedic_data[["2018"]]
+paramedic2017 <- paramedic_data[["2017"]]
+paramedic2016 <- paramedic_data[["2016"]]
+paramedic2015 <- paramedic_data[["2015"]]
+paramedic2014 <- paramedic_data[["2014"]]
+paramedic2013 <- paramedic_data[["2013"]]
+paramedic2012 <- paramedic_data[["2012"]]
+paramedic2011 <- paramedic_data[["2011"]]
+paramedic2010 <- paramedic_data[["2010"]]
 
 ### Save Data ###
 write_csv(paramedic2020, here("inputs", "data", "2020_paramedic-incidents_raw.csv"))
 write_csv(paramedic2019, here("inputs", "data", "2019_paramedic-incidents_raw.csv"))
+write_csv(paramedic2018, here("inputs", "data", "2018_paramedic-incidents_raw.csv"))
+write_csv(paramedic2017, here("inputs", "data", "2017_paramedic-incidents_raw.csv"))
+write_csv(paramedic2016, here("inputs", "data", "2016_paramedic-incidents_raw.csv"))
+write_csv(paramedic2015, here("inputs", "data", "2015_paramedic-incidents_raw.csv"))
+write_csv(paramedic2014, here("inputs", "data", "2014_paramedic-incidents_raw.csv"))
+write_csv(paramedic2013, here("inputs", "data", "2013_paramedic-incidents_raw.csv"))
+write_csv(paramedic2012, here("inputs", "data", "2012_paramedic-incidents_raw.csv"))
+write_csv(paramedic2011, here("inputs", "data", "2011_paramedic-incidents_raw.csv"))
+write_csv(paramedic2010, here("inputs", "data", "2010_paramedic-incidents_raw.csv"))
--- scripts/01_data-cleaning_paramedic-incidents.R
+++ scripts/01_data-cleaning_paramedic-incidents.R
@@ -8,10 +8,50 @@
 
 #### Workspace set-up ####
 library(tidyverse)
+library(here)
 
-## paramedic2020 ##
-# Reassigning Priority Codes back to dataset
-paramedic2020_cleaned <- paramedic2020 %>% 
+#### Read Data ####
+paramedic2020 <- read_csv(here("inputs","data","2020_paramedic-incidents_raw.csv"))
+paramedic2019 <- read_csv(here("inputs","data","2019_paramedic-incidents_raw.csv"))
+paramedic2018 <- read_csv(here("inputs","data","2018_paramedic-incidents_raw.csv"))
+paramedic2017 <- read_csv(here("inputs","data","2017_paramedic-incidents_raw.csv"))
+paramedic2016 <- read_csv(here("inputs","data","2016_paramedic-incidents_raw.csv"))
+paramedic2015 <- read_csv(here("inputs","data","2015_paramedic-incidents_raw.csv"))
+paramedic2014 <- read_csv(here("inputs","data","2014_paramedic-incidents_raw.csv"))
+paramedic2013 <- read_csv(here("inputs","data","2013_paramedic-incidents_raw.csv"))
+paramedic2012 <- read_csv(here("inputs","data","2012_paramedic-incidents_raw.csv"))
+paramedic2011 <- read_csv(here("inputs","data","2011_paramedic-incidents_raw.csv"))
+paramedic2010 <- read_csv(here("inputs","data","2010_paramedic-incidents_raw.csv"))
+
+## Standardizing Column Names ##
+
+paramedic2017 <- paramedic2017 %>%
+  rename(Forward_Sortation_Area = FSA)
+
+paramedic2011 <- paramedic2011 %>%
+  rename(Forward_Sortation_Area = FSA)
+
+paramedic2010 <- paramedic2010 %>%
+  rename(Forward_Sortation_Area = FSA)
+
+## Bind Data ##
+paramedic20192020_cleaned <- rbind(paramedic2019, paramedic2020)
+paramedic11years_cleaned <- rbind(paramedic2010, 
+                                  paramedic2011, 
+                                  paramedic2012, 
+                                  paramedic2013, 
+                                  paramedic2014,
+                                  paramedic2015,
+                                  paramedic2016,
+                                  paramedic2017, 
+                                  paramedic2018, 
+                                  paramedic2019, 
+                                  paramedic2019, 
+                                  paramedic2020)
+
+## Reassigning Priority Codes back to datasets ##
+### 2010-2020 ###
+paramedic11years_cleaned <- paramedic11years_cleaned %>%
   mutate(Priority_Code = case_when(
     Priority_Number == 1 ~ "Delta",
     Priority_Number == 3 ~ "Charlie",
@@ -23,7 +63,7 @@ paramedic2020_cleaned <- paramedic2020 %>%
     Priority_Number == 13 ~ "Alpha3")
   )
 
-paramedic2020_cleaned <- paramedic2020_cleaned %>% 
+paramedic11years_cleaned <- paramedic11years_cleaned %>% 
   mutate(Priority_Definition = case_when(
     Priority_Number == 1 ~ "Life Threatening, not Cardiac/Respiratory Arrest",
     Priority_Number == 3 ~ "Serious, not Life Threatening",
@@ -35,9 +75,8 @@ paramedic2020_cleaned <- paramedic2020_cleaned %>%
     Priority_Number == 13 ~ "Non Serious/Non Life Threatening, Minimal Intervention")
   )
 
-## paramedic2019 ##
-# Reassigning Priority Codes back to dataset
-paramedic2019_cleaned <- paramedic2019 %>% 
+### 2019-2020 ###
+paramedic20192020_cleaned <- paramedic20192020_cleaned %>% 
   mutate(Priority_Code = case_when(
     Priority_Number == 1 ~ "Delta",
     Priority_Number == 3 ~ "Charlie",
@@ -49,7 +88,7 @@ paramedic2019_cleaned <- paramedic2019 %>%
     Priority_Number == 13 ~ "Alpha3")
   )
 
-paramedic2019_cleaned <- paramedic2019_cleaned %>% 
+paramedic20192020_cleaned <- paramedic20192020_cleaned %>% 
   mutate(Priority_Definition = case_when(
     Priority_Number == 1 ~ "Life Threatening, not Cardiac/Respiratory Arrest",
     Priority_Number == 3 ~ "Serious, not Life Threatening",
@@ -61,10 +100,8 @@ paramedic2019_cleaned <- paramedic2019_cleaned %>%
     Priority_Number == 13 ~ "Non Serious/Non Life Threatening, Minimal Intervention")
   )
 
-### Bind Data ###
-paramedic20192020_cleaned <- rbind(paramedic2019_cleaned, paramedic2020_cleaned)
 
-### Additional Cleaning ###
+### Additional Cleaning (2019-2020) ###
 paramedic20192020_cleaned <- paramedic20192020_cleaned %>%
   mutate(Year = as.POSIXct(Dispatch_Time, format = "%m/%d/%Y %H:%M:%S")) %>% #Change datetime to POSIXct 
   mutate(Month = as.POSIXct(Dispatch_Time, format = "%m/%d/%Y %H:%M:%S")) #Change datetime to POSIXct 
@@ -87,9 +124,41 @@ paramedic20192020_cleaned <- paramedic20192020_cleaned %>% #Decided to transform
     Month == "11" ~ "Nov",
     Month == "12" ~ "Dec")
   )
-  
+
+paramedic20192020_cleaned$Month <- factor(paramedic20192020_cleaned$Month, levels = month.abb) #Turning Months into factors for easier sorting
+
+### Additional Cleaning (2010-2020) ###
+
+paramedic11years_cleaned <- paramedic11years_cleaned %>%
+  mutate(Year = as.POSIXct(Dispatch_Time, format = "%m/%d/%Y %H:%M:%S")) %>% #Change datetime to POSIXct 
+  mutate(Month = as.POSIXct(Dispatch_Time, format = "%m/%d/%Y %H:%M:%S")) #Change datetime to POSIXct 
+
+paramedic11years_cleaned$Year <- format(paramedic11years_cleaned$Year, format = "%Y") #Extract only year
+paramedic11years_cleaned$Month <- format(paramedic11years_cleaned$Month, format = "%m") #Extract only month
+
+paramedic11years_cleaned <- paramedic11years_cleaned %>% #Decided to transform string numbers into month abbreviations for better visualization purposes
+  mutate(Month = case_when(
+    Month == "01" ~ "Jan",
+    Month == "02" ~ "Feb",
+    Month == "03" ~ "Mar",
+    Month == "04" ~ "Apr",
+    Month == "05" ~ "May",
+    Month == "06" ~ "Jun",
+    Month == "07" ~ "Jul",
+    Month == "08" ~ "Aug",
+    Month == "09" ~ "Sep",
+    Month == "10" ~ "Oct",
+    Month == "11" ~ "Nov",
+    Month == "12" ~ "Dec")
+  )
+
+paramedic11years_cleaned$Month <- factor(paramedic11years_cleaned$Month, levels = month.abb) #Turning Month to factor for easier sorting
+paramedic11years_cleaned$Year <- as.character(paramedic11years_cleaned$Year) #Turning Year to character for graph manipulation
+
+### Duplicates Removal ###
+paramedic20192020_cleaned <- paramedic20192020_cleaned[!duplicated(paramedic20192020_cleaned$ID),]
+paramedic11years_cleaned <- paramedic11years_cleaned[!duplicated(paramedic11years_cleaned$ID),]
 
 ### Save Data ###
-write_csv(paramedic2020_cleaned, here("inputs", "data", "2020_paramedic-incidents_cleaned.csv"))
-write_csv(paramedic2019_cleaned, here("inputs", "data", "2019_paramedic-incidents_cleaned.csv"))
 write_csv(paramedic20192020_cleaned, here("inputs", "data", "2019-2020_paramedic-incidents_cleaned.csv"))
+write_csv(paramedic11years_cleaned, here("inputs", "data", "2010-2020_paramedic-incidents_cleaned.csv"))
