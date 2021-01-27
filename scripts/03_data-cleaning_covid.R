68a744e970653501c50b081da5fe38dcccea3cdc
Jeremy Chu <jeremychuj@gmail.com>
2021-01-27 18:17:49 -0500

    New outputs

--- scripts/03_data-cleaning_covid.R
+++ scripts/03_data-cleaning_covid.R
@@ -13,13 +13,13 @@ library(here)
 #### Read Data ####
 covid_df <- read_csv(here("inputs","data","covd-data_raw.csv"))
 
-# Filter for only hospitalized because the study's looking at medical emergencies
+# Filter for hospitalized because the study's mainly looking at medical emergencies
 
 covid_hospitalized <- 
   covid_df %>% 
   filter(`Ever Hospitalized` == "Yes")
 
-# Data cleaning
+# Data cleaning (Hospitalized)
 
 covid_hospitalized <- covid_hospitalized %>%
   mutate(Year = as.POSIXct(`Reported Date`, format = "%m/%d/%Y %H:%M:%S")) %>% #Change datetime to POSIXct 
@@ -58,7 +58,47 @@ covid_monthCount <-
 covid_monthCount <- covid_monthCount %>%
   rename(case_number = n) # Renaming for sanity
 
+# Data Cleaning (Full)
+
+covid_df <- covid_df %>%
+  mutate(Year = as.POSIXct(`Reported Date`, format = "%m/%d/%Y %H:%M:%S")) %>% #Change datetime to POSIXct 
+  mutate(Month = as.POSIXct(`Reported Date`, format = "%m/%d/%Y %H:%M:%S")) #Change datetime to POSIXct 
+
+covid_df$Year <- format(covid_df$Year, format = "%Y") #Extract only year
+covid_df$Month <- format(covid_df$Month, format = "%m") #Extract only month
+
+covid_df <- covid_df %>% #Decided to transform string numbers into month abbreviations for better visualization purposes
+  mutate(Month = case_when(
+    Month == "01" ~ "Jan",
+    Month == "02" ~ "Feb",
+    Month == "03" ~ "Mar",
+    Month == "04" ~ "Apr",
+    Month == "05" ~ "May",
+    Month == "06" ~ "Jun",
+    Month == "07" ~ "Jul",
+    Month == "08" ~ "Aug",
+    Month == "09" ~ "Sep",
+    Month == "10" ~ "Oct",
+    Month == "11" ~ "Nov",
+    Month == "12" ~ "Dec")
+  )
+
+covid_df$Month <- factor(covid_df$Month, levels = month.abb)
+
+# Creating another table that groups by month
+
+covid_fullmonthCount <-
+  covid_df %>%
+  filter(Year == 2020) %>%
+  group_by(Month) %>%
+  count() %>%
+  ungroup()
+
+covid_fullmonthCount <- covid_fullmonthCount %>%
+  rename(case_number = n) # Renaming for sanity
+
 ### Writing Data ###
 
 write_csv(covid_monthCount, here("inputs", "data", "2020_covid-cases_cleaned.csv"))
 write_csv(covid_hospitalized, here("inputs", "data", "2020_covid-cases-hospitalization_cleaned.csv"))
+write_csv(covid_fullmonthCount, here("inputs", "data", "2020_covid-cases-full_cleaned.csv"))
