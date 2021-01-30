637cf66bf4b72ff22c0d20e74e37c0cf0182ccc4
Jeremy Chu <jeremychuj@gmail.com>
2021-01-30 13:48:09 -0500

    Updated code

--- scripts/05_extra_covid-echo-mapping.R
+++ scripts/05_extra_covid-echo-mapping.R
@@ -2,7 +2,7 @@
 # Purpose: Generate geomaps of paramedic-incidents and covid-19 datasets
 # Author: Jeremy Chu
 # Contact: jeremychuj@gmail.com
-# Date: 22 January 2021
+# Date: 30 January 2021
 # Pre-requisites: Run 00-04 R scripts. Have covid_hospitalized and paramedic20192020_cleaned data in Global Environment. Have fsas shape data loaded.
 #TODOs: Create a geomap that is saved as a png.
 
@@ -31,13 +31,14 @@ paramedic20192020_cleaned <- read_csv(here("inputs", "data", "2019-2020_paramedi
 ## Optional - Reading Shapefile Locally  ##
 #fsas <-  readOGR(dsn = path.expand(here("inputs/shapefiles/lfsa000b16a_e.shp")), layer = "lfsa000b16a_e") #Read shapefile locally, note that you need all the other accompanying files to read it in, otherwise you get an error
 
-### Manipulation of Shapefile ###
-toronto.fsas <- fsas[substr(fsas$CFSAUID, 1, 1) == 'M',] #Extracts Toronto Postal Codes
-data <- ggplot2::fortify(toronto.fsas, region = "CFSAUID") #Turning object into something plottable by ggplot
-data$fsa <- factor(data$id)
-data$id <- NULL
+## Manipulation of Shapefile ##
+toronto.fsas <- fsas[substr(fsas$CFSAUID, 1, 1) == 'M',] # Extracts Toronto Postal Codes
+data <- ggplot2::fortify(toronto.fsas, region = "CFSAUID") # Turning object into something plottable by ggplot
+data$fsa <- factor(data$id) # Turns the id column into a factored fsa
+data$id <- NULL # Removes the id column
 
-### Transforming data in preparation for merging ###
+## Cleanup ##
+rm(fsas) # fsas is quite large, removing to speed things up
 
 ## Transforming data in preparation for merging ##
 
@@ -68,7 +69,6 @@ paramedic2019_map_allmonths <- paramedic20192020_cleaned %>% # Creating a table 
   rename(fsa = Forward_Sortation_Area) %>% # rename Forward_Sortation_Area to fsa to join later
   rename(echo2019cases = n) #rename n into something more understandable
 
-
 plot.data_para_2019 <- left_join(data, paramedic2019_map_allmonths) # Merge the location data with paramedic data, joining under the "fsa" column. left_join is used because merge causes polygon tearing.
 
 ## Plotting the Maps ##
@@ -85,7 +85,7 @@ covid_map <- ggplot(plot.data_covid_allmonths, aes(x = long, y = lat, group = gr
         legend.title = element_blank(),
         plot.title = element_text(size = 10, face = "bold", hjust = 0.5),
         plot.caption = element_text(size = 6)) +
-  labs(title = "Toronto COVID-19 Cases Heatmap",
+  labs(title = "Toronto COVID-19 Hospitalization Heatmap",
        caption ="Source: Open Data Toronto")  
 
 # Paramedic Map (2020)
@@ -105,7 +105,6 @@ paramedic_map <- ggplot(plot.data_para_allmonths, aes(x = long, y = lat, group =
        caption ="Source: Open Data Toronto")  
 
 # Paramedic Map (2019)
-
 map2019 <- ggplot(plot.data_para_2019, aes(x = long, y = lat, group = group, fill = echo2019cases)) +
   geom_polygon() +
   coord_equal() +
@@ -119,7 +118,7 @@ map2019 <- ggplot(plot.data_para_2019, aes(x = long, y = lat, group = group, fil
         plot.title = element_text(size = 10, face = "bold", hjust = 0.5),
         plot.caption = element_text(size = 6)) +
   labs(title = "Toronto 2019 Echo Incidents Heatmap",
-       caption ="Source: Open Data Toronto")  
+       caption ="Source: Open Data Toronto")   
 
 #Putting 2020 Echo and COVID into 1 Panel
 covid_echo_mapping_panel <-
@@ -128,4 +127,3 @@ covid_echo_mapping_panel <-
 #Saving Data
 ggsave(covid_echo_mapping_panel, file = here("outputs/paper/images/06_2020-covid-echo-mapping-panel.png"), width = 8, height = 5)
 ggsave(map2019, file = here("outputs/paper/images/07_2019-echo-heatmap.png"), width = 8, height = 5)
-
